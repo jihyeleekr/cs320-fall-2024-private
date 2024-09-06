feecc58d2e777c7b7b39c6b417fc66ba51e75e78
Jihye Lee <102567820+jihyeleekr@users.noreply.github.com>
2024-09-06 19:08:25 -0400

    Update test_assign01.ml

--- assigns/assign01/test/test_assign01.ml
+++ assigns/assign01/test/test_assign01.ml
@@ -3,9 +3,9 @@ open OUnit2
 let tests =
   "Testing Assign01" >:::
     [ Test01.tests
-    (*; Test02.tests
+    (* ; Test02.tests
     ; Test03.tests
-    ; Test04.tests*)
+    ; Test04.tests *)
     ]
 
 let _ = run_test_tt_main tests
