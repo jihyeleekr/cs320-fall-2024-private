22c3fa48d58953cd9695ead578a907c94edf70dc
Jihye Lee <jihye020211@gmail.com>
2024-09-07 20:56:04 -0400

    assign01_03 and assign01_04 added

---
+++ assigns/assign01/lib/assign01_03.ml
@@ -0,0 +1,13 @@
+open Assign01_02
+
+let nth (s: int) (i : int) : int =
+  let x = nth_prime i in
+  let rec is_div (s : int) (x : int) (c : int) : int = 
+    if s mod x <> 0 then c
+    else is_div (s / x)  x (c + 1)
+  in
+  if s < 0 then failwith "Invalid input for s"
+  else is_div s x 0
+;;
+
+nth 1058400 0;;
