e7bbb117fc00bde2b3ec31d1ebfdb7f70b421e76
Jihye Lee <jihye020211@gmail.com>
2024-09-07 21:00:04 -0400

    test case update

--- assigns/assign01/test/test_assign01.ml
+++ assigns/assign01/test/test_assign01.ml
@@ -4,8 +4,8 @@ let tests =
   "Testing Assign01" >:::
     [ Test01.tests
     ; Test02.tests
-    (*; Test03.tests
-    ; Test04.tests *)
+    ; Test03.tests
+    ; Test04.tests
     ]
 
 let _ = run_test_tt_main tests
