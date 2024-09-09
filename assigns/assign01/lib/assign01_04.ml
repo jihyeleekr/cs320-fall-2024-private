68326d642cdf9202af5758f548afdb14b9507477
Jihye Lee <jihye020211@gmail.com>
2024-09-08 22:14:36 -0400

    assing01_04 updated

--- assigns/assign01/lib/assign01_04.ml
+++ assigns/assign01/lib/assign01_04.ml
@@ -1,13 +1,13 @@
 open Assign01_03
 let to_string (s : int) : string =
-  let rec generate_sequence s i seq = (* Return the number sequences in list form: contains int*)
+  let rec generate_sequence (s : int) (i : int) (seq : int list) : int list = (* Return the number sequences in list form: contains int*)
     let count = nth s i in 
     if count = 0 then List.rev seq (* Reverse the list *)
     else generate_sequence s (i + 1) (count :: seq)
   in 
   let seq_list = generate_sequence s 0 [] in
   
-  let list_to_string lst = 
+  let list_to_string (lst : int list) : string = 
     let rec aux lst = (* Take the list elements and convert it into string *)
       match lst with
       | [] -> ""
