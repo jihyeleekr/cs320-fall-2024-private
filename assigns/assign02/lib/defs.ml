1da6a5a0712936d3fc169765c8a4d29dbb3e99fb
Jihye Lee <jihye020211@gmail.com>
2024-09-14 15:51:57 -0400

    o

---
+++ assigns/assign02/lib/defs.ml
@@ -0,0 +1,21 @@
+type piece = 
+| X
+| O
+
+type pos = 
+| Piece of piece
+| Blank
+
+type board = (pos * pos * pos) * (pos * pos * pos) * (pos * pos * pos)
+
+type row_index = 
+| Top
+| Middle
+| Bottom
+
+type col_index = 
+| Left
+| Middle 
+| Right
+
+type pos_index = row_index * col_index
