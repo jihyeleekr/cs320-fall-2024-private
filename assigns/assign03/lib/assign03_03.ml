8e0ebb820c7a6ed49e8c29de0421e2c455c0afbf
Jihye Lee <jihye020211@gmail.com>
2024-09-24 12:28:08 -0400

    t

--- assigns/assign03/lib/assign03_03.ml
+++ assigns/assign03/lib/assign03_03.ml
@@ -1,14 +1,10 @@
 type tree = 
   | Leaf of int
   | Node of tree list
-
-(* Helper function to concatenate a list of lists *)
-let rec concat_lists lsts =
+let rec connect lsts =
   match lsts with
   | [] -> []
-  | l :: ls -> l @ concat_lists ls
-
-(* Helper function to compute the height of the tree *)
+  | l :: ls -> l @ connect ls
 let rec height t =
   match t with
   | Leaf _ -> 0
@@ -19,22 +15,19 @@ let rec height t =
       | c :: cs -> max (height c) (max_depth cs)
     in 1 + max_depth cs
 
-(* Helper function to collect all terminal elements from a tree *)
 let rec terminal_elements t =
   match t with
   | Leaf _ -> [t]
   | Node [] -> [t]
-  | Node children -> concat_lists (List.map terminal_elements children)
+  | Node children -> connect (List.map terminal_elements children)
 
-(* Main collapse function *)
 let rec collapse h t =
   if h = 0 then t
   else match t with
   | Leaf _ -> t
   | Node children ->
     if h = 1 then
-      (* Collapse the children to terminal elements *)
-      Node (concat_lists (List.map terminal_elements children))
+      Node (connect (List.map terminal_elements children))
     else
       (* Recursively collapse each child node *)
       Node (List.map (collapse (h - 1)) children)
