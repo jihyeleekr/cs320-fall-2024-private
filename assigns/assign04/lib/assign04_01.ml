75c04bbd5e58466fbd9c93139ac36b3facace648
Jihye Lee <jihye020211@gmail.com>
2024-09-28 14:07:09 -0400

    assing04 update

--- assigns/assign04/lib/assign04_01.ml
+++ assigns/assign04/lib/assign04_01.ml
@@ -1,8 +1,9 @@
 
 let large_value = max_int
 let rec lifespan (f : 'a -> 'a) (s : 'a) (p: ('a -> bool)) (k : int) : int = 
-  if p(f s) then k
-  else lifespan f (f s) p k + 1
+  if k > 1000000 then large_value  
+  else if p (f s) then k
+  else lifespan f (f s) p (k + 1)
 
   let calculate_lifespan f s p =
     let rec aux current_s count =
@@ -12,15 +13,15 @@ let rec lifespan (f : 'a -> 'a) (s : 'a) (p: ('a -> bool)) (k : int) : int =
     in
     aux s 0
   
-  let last_function_standing fs s p =
-    let rec max_lifespan_finder fs max_f max_lifespan =
-      match fs with
+  let last_function_standing funcs start pred =
+    let rec max_lifespan_finder funcs max_f max_lifespan =
+      match funcs with
       | [] -> max_f  
       | f :: rest ->
-        let l = calculate_lifespan f s p in
+        let l = calculate_lifespan f start pred in
         if l = large_value then Some f  
         else if l > max_lifespan then max_lifespan_finder rest (Some f) l  
         else if l = max_lifespan then None  
         else max_lifespan_finder rest max_f max_lifespan  
     in
-    max_lifespan_finder fs None 0
+    max_lifespan_finder funcs None 0
