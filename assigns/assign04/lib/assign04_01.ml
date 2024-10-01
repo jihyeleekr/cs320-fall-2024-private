9dd340ec25b21824904a722b826644b86f66a71b
Jihye Lee <jihye020211@gmail.com>
2024-09-30 20:10:01 -0400

    assing04_01 update

--- assigns/assign04/lib/assign04_01.ml
+++ assigns/assign04/lib/assign04_01.ml
@@ -1,39 +1,44 @@
 let large_value = max_int
 
-let rec lifespan (f : 'a -> 'a) (s : 'a) (p : ('a -> bool)) (k : int) : int option =
+let rec lifespan (f : 'a -> 'a) (s : 'a) (p: ('a -> bool)) (k : int) : int option =
   if k > 1000000 then Some large_value  
   else if p (f s) then Some k
   else lifespan f (f s) p (k + 1)
 
-let rec infinite_lifespan_check funcs start pred =
-  match funcs with
-  | [] -> 0  
-  | f :: rest ->
-    match lifespan f start pred 0 with
-    | Some l when l = large_value -> 1 + infinite_lifespan_check rest start pred  
-    | _ -> infinite_lifespan_check rest start pred  
+let calculate_lifespan f s p =
+  let rec aux current_s count =
+    if p current_s then Some count  
+    else if count > 1000000 then Some large_value
+    else aux (f current_s) (count + 1)  
+  in
+  aux s 0
 
 let last_function_standing funcs start pred =
-  match funcs with
-  | [] -> None  
-  | [f] -> Some f  
-  | _ ->
-    let rec max_lifespan_finder funcs max_f max_lifespan =
-      match funcs with
-      | [] -> max_f  
-      | f :: rest ->
-        match lifespan f start pred 0 with
-        | None -> max_lifespan_finder rest max_f max_lifespan  
-        | Some l ->
-          if l = large_value then
-            let infinite_count = infinite_lifespan_check rest start pred in
-            if infinite_count > 0 then None  
-            else max_lifespan_finder rest (Some f) max_lifespan 
-          else if l > max_lifespan then
-            max_lifespan_finder rest (Some f) l
-          else if l = max_lifespan then
-            None  
-          else
-            max_lifespan_finder rest max_f max_lifespan  
-    in
-    max_lifespan_finder funcs None 0
+  let rec max_lifespan_finder funcs max_f max_lifespan =
+    match funcs with
+    | [] -> max_f  
+    | f :: rest ->
+      match calculate_lifespan f start pred with
+      | None -> max_lifespan_finder rest max_f max_lifespan  (* Skip failed lifespans *)
+      | Some l ->
+        if l = large_value then
+          (* Count the number of functions with infinite lifespan *)
+          let rec count_infinite rest =
+            match rest with
+            | [] -> 0
+            | f' :: rest' ->
+              match calculate_lifespan f' start pred with
+              | Some l' when l' = large_value -> 1 + count_infinite rest'
+              | _ -> count_infinite rest'
+          in
+          let count = count_infinite rest in
+          if count > 0 then None  (* Multiple infinite lifespan functions found *)
+          else max_lifespan_finder rest (Some f) max_lifespan  (* Continue checking *)
+        else if l > max_lifespan then
+          max_lifespan_finder rest (Some f) l
+        else if l = max_lifespan then
+          None  (* Found a tie *)
+        else
+          max_lifespan_finder rest max_f max_lifespan  
+  in
+  max_lifespan_finder funcs None 0
