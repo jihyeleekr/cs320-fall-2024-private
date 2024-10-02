1571548c1f1977b90cf46f6676860510d60e3001
Jihye Lee <jihye020211@gmail.com>
2024-10-02 11:26:42 -0400

    assign04_01 update

--- assigns/assign04/lib/assign04_01.ml
+++ assigns/assign04/lib/assign04_01.ml
@@ -1,26 +1,28 @@
+let large_value = max_int
+
+let calculate_lifespan f s p =
+  let rec aux current_s count =
+    if p current_s then count  
+    else if count > 1000000 then large_value
+    else aux (f current_s) (count + 1)  
+  in
+  aux s 0
 
 let last_function_standing funcs start pred =
-  let rec compare_funcs funcs max_f current_s max_count =
+  let rec max_lifespan_finder funcs max_f max_lifespan max_count =
     match funcs with
     | [] -> 
-      if max_count > 1 then None
-      else max_f
+      if max_count > 1 then None  
+      else max_f  
     | f :: rest ->
-      let rec step f current_s step_count =
-        if pred current_s then step_count
-        else step f (f current_s) (step_count + 1)
-      in
-      let f_steps = step f start 0 in
-      match max_f with
-      | None -> 
-        compare_funcs rest (Some f) start 1
-      | Some max_f' ->
-        let max_steps = step max_f' start 0 in
-        if f_steps > max_steps then
-          compare_funcs rest (Some f) start 1
-        else if f_steps = max_steps then
-          compare_funcs rest max_f current_s (max_count + 1)
-        else
-          compare_funcs rest max_f current_s max_count
+      let l = calculate_lifespan f start pred in
+      if l = large_value then
+        max_lifespan_finder rest (Some f) max_lifespan max_count  
+      else if l > max_lifespan then
+        max_lifespan_finder rest (Some f) l 1  
+      else if l = max_lifespan then
+        max_lifespan_finder rest max_f max_lifespan (max_count + 1)  
+      else
+        max_lifespan_finder rest max_f max_lifespan max_count  
   in
-  compare_funcs funcs None start 0
+  max_lifespan_finder funcs None 0 0
