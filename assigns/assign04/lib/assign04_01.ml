750b7218f9a0b80d8445fe3d19ed6b281c109f95
Jihye Lee <jihye020211@gmail.com>
2024-10-02 11:32:15 -0400

    assign04_01 update

--- assigns/assign04/lib/assign04_01.ml
+++ assigns/assign04/lib/assign04_01.ml
@@ -1,28 +1,26 @@
-let large_value = max_int
 
 let calculate_lifespan f s p =
   let rec aux current_s count =
     if p current_s then count  
-    else if count > 1000000 then large_value
     else aux (f current_s) (count + 1)  
   in
-  aux s 0
+  aux s 0  
 
 let last_function_standing funcs start pred =
-  let rec max_lifespan_finder funcs max_f max_lifespan max_count =
+  let rec find_max_lifespan funcs current_max_f current_max_lifespan tied_count =
     match funcs with
     | [] -> 
-      if max_count > 1 then None  
-      else max_f  
+      if tied_count > 1 then None  
+      else current_max_f  
     | f :: rest ->
-      let l = calculate_lifespan f start pred in
-      if l = large_value then
-        max_lifespan_finder rest (Some f) max_lifespan max_count  
-      else if l > max_lifespan then
-        max_lifespan_finder rest (Some f) l 1  
-      else if l = max_lifespan then
-        max_lifespan_finder rest max_f max_lifespan (max_count + 1)  
+      let lifespan = calculate_lifespan f start pred in  
+      if lifespan > current_max_lifespan then
+        find_max_lifespan rest (Some f) lifespan 1
+      else if lifespan = current_max_lifespan then
+        find_max_lifespan rest current_max_f current_max_lifespan (tied_count + 1)
       else
-        max_lifespan_finder rest max_f max_lifespan max_count  
+        find_max_lifespan rest current_max_f current_max_lifespan tied_count
   in
-  max_lifespan_finder funcs None 0 0
+  find_max_lifespan funcs None (-1) 0  
+
+
