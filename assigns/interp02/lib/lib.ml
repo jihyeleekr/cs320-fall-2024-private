aad922e5e20c3da306eb5dfe4845a5130ecd1a7d
Jihye Lee <jihye020211@gmail.com>
2024-11-21 22:04:11 -0500

    update lib.ml

--- assigns/interp02/lib/lib.ml
+++ assigns/interp02/lib/lib.ml
@@ -11,25 +11,27 @@ exception AssertFail
 let desugar (prog : prog) : expr =
     let rec desugar_toplets = function
       | [] -> Unit  
-      | { is_rec; name; args; ty; value } :: rest ->
+      | [{ is_rec; name; args; ty; value }] ->
           let desugared_value =
             List.fold_right
-              (fun (arg, arg_ty) acc -> Fun (arg, arg_ty, acc))  
+              (fun (arg, arg_ty) acc -> Fun (arg, arg_ty, acc))
               args
               (desugar_expr value)
           in
-          Let {
-            is_rec;
-            name;
-            ty;
-            value = desugared_value;
-            body = desugar_toplets rest;  
-          }
+          Let { is_rec; name; ty; value = desugared_value; body = Unit }
+      | { is_rec; name; args; ty; value } :: rest ->
+          let desugared_value =
+            List.fold_right
+              (fun (arg, arg_ty) acc -> Fun (arg, arg_ty, acc))
+              args
+              (desugar_expr value)
+          in
+          Let { is_rec; name; ty; value = desugared_value; body = desugar_toplets rest }
     and desugar_expr = function
       | SLet { is_rec; name; args; ty; value; body } ->
           let desugared_value =
             List.fold_right
-              (fun (arg, arg_ty) acc -> Fun (arg, arg_ty, acc))  
+              (fun (arg, arg_ty) acc -> Fun (arg, arg_ty, acc))
               args
               (desugar_expr value)
           in
@@ -42,26 +44,24 @@ let desugar (prog : prog) : expr =
           }
       | SFun { arg; args; body } ->
           List.fold_right
-            (fun (arg, arg_ty) acc -> Fun (arg, arg_ty, acc))  
+            (fun (arg, arg_ty) acc -> Fun (arg, arg_ty, acc))
             (arg :: args)
-            (desugar_expr body)
+            (desugar_expr body)  
       | SIf (cond, then_, else_) ->
-          If (desugar_expr cond, desugar_expr then_, desugar_expr else_)  
+          If (desugar_expr cond, desugar_expr then_, desugar_expr else_)
       | SApp (e1, e2) ->
-          App (desugar_expr e1, desugar_expr e2)  
+          App (desugar_expr e1, desugar_expr e2)
       | SBop (op, e1, e2) ->
-          Bop (op, desugar_expr e1, desugar_expr e2)  
+          Bop (op, desugar_expr e1, desugar_expr e2)
       | SAssert e ->
-          Assert (desugar_expr e)  
-      | SUnit -> Unit  
-      | STrue -> True  
-      | SFalse -> False  
-      | SNum n -> Num n  
-      | SVar x -> Var x  
+          Assert (desugar_expr e)
+      | SUnit -> Unit
+      | STrue -> True
+      | SFalse -> False
+      | SNum n -> Num n
+      | SVar x -> Var x
     in
     desugar_toplets prog
-  
-  
 (* Type checking *)
 let type_of (expr : expr) : (ty, error) result =
     let rec typecheck env expr =
@@ -136,57 +136,65 @@ let type_of (expr : expr) : (ty, error) result =
   
 (* Evaluation *)
 let eval (expr : expr) : value =
-  let rec eval_expr env expr =
-    match expr with
-    | Unit -> VUnit
-    | Num n -> VNum n
-    | True -> VBool true
-    | False -> VBool false
-    | Var x -> Env.find x env
-    | Let { is_rec; name; ty = _ty; value; body } -> 
-        let closure_env =
-          if is_rec then
-            Env.add name (VClos { name = Some name; arg = ""; body = value; env }) env
-          else env
-        in
-        let v = eval_expr closure_env value in
-        let new_env = Env.add name v closure_env in
-        eval_expr new_env body
-    | Fun (arg, _, body) -> VClos { name = None; arg; body; env }
-    | App (e1, e2) -> (
+    let rec eval_expr env expr =
+      match expr with
+      | Unit -> VUnit
+      | Num n -> VNum n
+      | True -> VBool true
+      | False -> VBool false
+      | Var x -> Env.find x env
+      | Let { is_rec; name; ty = _; value; body } ->
+          let closure_env =
+            if is_rec then
+              Env.add name (VClos { name = Some name; arg = ""; body = value; env }) env
+            else env
+          in
+          let v = eval_expr closure_env value in
+          let extended_env = Env.add name v closure_env in
+          eval_expr extended_env body
+      | Fun (arg, _, body) -> VClos { name = None; arg; body; env }
+      | App (e1, e2) -> (
         match eval_expr env e1 with
-        | VClos { name = _name; arg; body; env = env' } -> 
-            let v = eval_expr env e2 in
-            let extended_env = Env.add arg v env' in
+        | VClos { name = _; arg; body; env = closure_env } ->
+            let v2 = eval_expr env e2 in
+            let extended_env = Env.add arg v2 closure_env in
             eval_expr extended_env body
-        | _ -> assert false)
-    | If (cond, then_, else_) ->
-        let cond_val = eval_expr env cond in
-        if cond_val = VBool true then eval_expr env then_ else eval_expr env else_
-    | Bop (op, e1, e2) -> (
-        let v1 = eval_expr env e1 in
-        let v2 = eval_expr env e2 in
-        match (v1, v2, op) with
-        | VNum n1, VNum n2, Add -> VNum (n1 + n2)
-        | VNum n1, VNum n2, Sub -> VNum (n1 - n2)
-        | VNum n1, VNum n2, Mul -> VNum (n1 * n2)
-        | VNum n1, VNum n2, Div ->
-            if n2 = 0 then raise DivByZero else VNum (n1 / n2)
-        | VNum n1, VNum n2, Mod -> VNum (n1 mod n2)
-        | VNum n1, VNum n2, Lt -> VBool (n1 < n2)
-        | VNum n1, VNum n2, Lte -> VBool (n1 <= n2)
-        | VNum n1, VNum n2, Gt -> VBool (n1 > n2)
-        | VNum n1, VNum n2, Gte -> VBool (n1 >= n2)
-        | VNum n1, VNum n2, Eq -> VBool (n1 = n2)
-        | VNum n1, VNum n2, Neq -> VBool (n1 <> n2)
-        | VBool b1, VBool b2, And -> VBool (b1 && b2)
-        | VBool b1, VBool b2, Or -> VBool (b1 || b2)
-        | _ -> assert false)
-    | Assert e ->
-        let v = eval_expr env e in
-        if v = VBool true then VUnit else raise AssertFail
-  in
-  eval_expr Env.empty expr
+        | _ -> assert false
+    )
+        | If (cond, then_, else_) -> (
+            match eval_expr env cond with
+            | VBool b ->
+                if b then eval_expr env then_ else eval_expr env else_
+            | _ -> assert false
+        )
+      | Bop (op, e1, e2) ->
+          let v1 = eval_expr env e1 in
+          let v2 = eval_expr env e2 in
+          begin match (v1, v2, op) with
+          | VNum n1, VNum n2, Add -> VNum (n1 + n2)
+          | VNum n1, VNum n2, Sub -> VNum (n1 - n2)
+          | VNum n1, VNum n2, Mul -> VNum (n1 * n2)
+          | VNum n1, VNum n2, Div -> VNum (n1 / n2)
+          | VNum n1, VNum n2, Mod -> VNum (n1 mod n2)
+          | VNum n1, VNum n2, Lt -> VBool (n1 < n2)
+          | VNum n1, VNum n2, Lte -> VBool (n1 <= n2)
+          | VNum n1, VNum n2, Gt -> VBool (n1 > n2)
+          | VNum n1, VNum n2, Gte -> VBool (n1 >= n2)
+          | VNum n1, VNum n2, Eq -> VBool (n1 = n2)
+          | VNum n1, VNum n2, Neq -> VBool (n1 <> n2)
+          | VBool b1, VBool b2, And -> VBool (b1 && b2)
+          | VBool b1, VBool b2, Or -> VBool (b1 || b2)
+          | _ -> assert false
+          end
+        | Assert e -> (
+        match eval_expr env e with
+        | VBool b ->
+            if b then VUnit else raise AssertFail
+        | _ -> assert false
+        )
+    in
+    eval_expr Env.empty expr
+  
 
   let interp (input : string) : (value, error) result =
     match parse input with
