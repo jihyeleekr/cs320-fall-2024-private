a857d1bdb5103f87e45e644ea0927df005438a95
Jihye Lee <jihye020211@gmail.com>
2024-11-25 12:00:05 -0500

    update lib.ml

--- assigns/interp02/lib/lib.ml
+++ assigns/interp02/lib/lib.ml
@@ -125,6 +125,12 @@ let type_of (expr : expr) : (ty, error) result =
                     | Add | Sub | Mul | Div | Mod when ty1 = IntTy && ty2 = IntTy -> Ok IntTy
                     | Lt | Lte | Gt | Gte | Eq | Neq when ty1 = IntTy && ty2 = IntTy -> Ok BoolTy
                     | And | Or when ty1 = BoolTy && ty2 = BoolTy -> Ok BoolTy
+                    | Add | Sub | Mul | Div | Mod when ty1 <> IntTy -> Error (OpTyErrL (op, IntTy, ty1))
+                    | Add | Sub | Mul | Div | Mod when ty2 <> IntTy -> Error (OpTyErrR (op, IntTy, ty2))
+                    | Lt | Lte | Gt | Gte | Eq | Neq when ty1 <> IntTy -> Error (OpTyErrL (op, IntTy, ty1))
+                    | Lt | Lte | Gt | Gte | Eq | Neq when ty2 <> IntTy -> Error (OpTyErrR (op, IntTy, ty2))
+                    | And | Or when ty1 <> BoolTy -> Error (OpTyErrL (op, BoolTy, ty1))
+                    | And | Or when ty2 <> BoolTy -> Error (OpTyErrR (op, BoolTy, ty2))
                     | _ -> Error (OpTyErrL (op, ty1, ty2))
                 )
                 | Error e -> Error e
@@ -139,7 +145,6 @@ let type_of (expr : expr) : (ty, error) result =
         )
     in
     typecheck Env.empty expr
-
 
 
 (* Evaluation *)
