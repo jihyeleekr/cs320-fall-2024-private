5f1f6a1872a0803196469d741dda3f3f6b0ead12
Jihye Lee <jihye020211@gmail.com>
2024-12-05 21:34:55 -0500

    update lib.ml

--- assigns/interp03/lib/lib.ml
+++ assigns/interp03/lib/lib.ml
@@ -84,8 +84,8 @@ let rec type_of env expr =
       let arg_ty = TVar (gensym ()) in
       let res_ty = TVar (gensym ()) in
       (match type_of env f, type_of env arg with
-       | Some (Forall (_, TFun (param_ty, ret_ty))), Some (Forall (_, actual_arg_ty)) ->
-           let constraints = [(param_ty, arg_ty); (arg_ty, actual_arg_ty); (res_ty, ret_ty)] in
+       | Some (Forall (_, f_ty)), Some (Forall (_, actual_arg_ty)) ->
+           let constraints = [(f_ty, TFun (arg_ty, res_ty)); (arg_ty, actual_arg_ty)] in
            (match unify res_ty constraints with
             | Some (Forall (_, unified_res_ty)) -> Some (Forall ([], unified_res_ty))
             | None -> None)
@@ -103,7 +103,8 @@ let rec type_of env expr =
       match op with
       | Add | Sub | Mul | Div | Mod -> (TInt, TInt)
       | AddF | SubF | MulF | DivF | PowF -> (TFloat, TFloat)
-      | Lt | Lte | Gt | Gte | Eq | Neq -> (TBool, TVar (gensym ()))
+      | Lt | Lte | Gt | Gte -> (TBool, TInt)
+      | Eq | Neq -> (TBool, TVar (gensym ())) 
       | And | Or -> (TBool, TBool)
       | _ -> (TVar (gensym ()), TVar (gensym ()))
     in
@@ -121,11 +122,12 @@ let rec type_of env expr =
   | Let { is_rec = true; name; value; body } ->
     (match value with
      | Fun (arg, _, body_fun) ->
-       let arg_ty = TVar (gensym ()) in
+       let arg_ty1 = TVar (gensym ()) in
+       let arg_ty2 = TVar (gensym ()) in
        let ret_ty = TVar (gensym ()) in
-       let fun_ty = TFun (arg_ty, ret_ty) in
+       let fun_ty = TFun (arg_ty1, TFun (arg_ty2, ret_ty)) in
        let env' = Env.add name (Forall ([], fun_ty)) env in
-       (match type_of (Env.add arg (Forall ([], arg_ty)) env') body_fun with
+       (match type_of (Env.add arg (Forall ([], arg_ty1)) env') body_fun with
         | Some (Forall (_, inferred_ret_ty)) ->
           let constraints = [(ret_ty, inferred_ret_ty)] in
           (match unify fun_ty constraints with
@@ -173,6 +175,7 @@ let rec type_of env expr =
         | None -> None)
      | _ -> None)
   | _ -> None
+
 
 exception AssertFail
 exception DivByZero
